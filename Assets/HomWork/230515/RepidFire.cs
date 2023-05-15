6f02cef748924c44b833f1417ae4b1d7b13c8c5f
Daighter <arkansiel@naver.com>
2023-05-15 21:13:42 +0900

    feat : 1, 2, 3 과제 구현

---
+++ Assets/HomWork/230515/RepidFire.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RepidFire : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform firePosition;
+    private void OnFire(InputValue value)
+    {
+        Instantiate(bullet, firePosition.position, transform.rotation);
+    }
+
+    [SerializeField] float repeatTime = 1f;
+    private Coroutine BulletRoutine;
+    IEnumerator BulletMakeRoutine()
+    {
+        while (true)
+        {
+            Instantiate(bullet, firePosition.position, transform.rotation);
+            yield return new WaitForSeconds(repeatTime);
+        }
+    }
+
+    private void OnRepidFire(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            BulletRoutine = StartCoroutine(BulletMakeRoutine());
+        }
+        else
+        {
+            StopCoroutine(BulletRoutine);
+        }
+    }
+}
