66c45b9bed23f10a14cd479d8176fb663d822f05
Daighter <arkansiel@naver.com>
2023-05-16 19:38:18 +0900

    feat : 총알 충돌 시 이펙트와 레이어 작성

--- Assets/Scripts/BulletMove1.cs
+++ Assets/Scripts/BulletMove1.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class BulletMove1 : MonoBehaviour
 {
+    private Rigidbody rb;
     [SerializeField] float speed = 5f;
-    private void Update()
+    private void Awake()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        rb = GetComponent<Rigidbody>();
+    }
 
+    private void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, 5f);
+    }
 
+    [SerializeField] private GameObject ExplosionEffect;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(ExplosionEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
